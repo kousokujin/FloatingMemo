6adee3522a1787f87fd746d0693c0372b4b4b4dd
kousokujin <tomonori05@gmail.com>
2015-10-07 23:25:29 +0900

    ウィンドウのインスタンスの変数を配列で動的に作ってウインドウいっぱい作りたい

---
+++ FloatingMemo/NotifyIconWrapper.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace FloatingMemo
+{
+    partial class NotifyIconWrapper
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotifyIconWrapper));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exit_app = new System.Windows.Forms.ToolStripMenuItem();
+            this.Add_new_memo = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyMenu.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.notifyMenu;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // notifyMenu
+            // 
+            this.notifyMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Add_new_memo,
+            this.exit_app});
+            this.notifyMenu.Name = "notifyMenu";
+            this.notifyMenu.Size = new System.Drawing.Size(156, 48);
+            // 
+            // exit_app
+            // 
+            this.exit_app.Name = "exit_app";
+            this.exit_app.Size = new System.Drawing.Size(155, 22);
+            this.exit_app.Text = "終了";
+            // 
+            // Add_new_memo
+            // 
+            this.Add_new_memo.AutoSize = false;
+            this.Add_new_memo.Name = "Add_new_memo";
+            this.Add_new_memo.Size = new System.Drawing.Size(155, 22);
+            this.Add_new_memo.Text = "新しいメモを追加";
+            this.notifyMenu.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip notifyMenu;
+        private System.Windows.Forms.ToolStripMenuItem exit_app;
+        private System.Windows.Forms.ToolStripMenuItem Add_new_memo;
+    }
+}
---
+++ FloatingMemo/NotifyIconWrapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatingMemo
+{
+    public partial class NotifyIconWrapper : Component
+    {
+
+        List<memo_window> memo_list;
+
+        //---------以下イベント------------------
+        public NotifyIconWrapper()
+        {
+            InitializeComponent();
+
+            this.exit_app.Click += this.exit_app_click; //終了イベント
+            this.Add_new_memo.Click += this.add_memo_click; //新規メモ作成イベント
+        }
+
+        public NotifyIconWrapper(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        private void exit_app_click(object sender,EventArgs e)  //タスクバーの終了ボタン
+        {
+            App.Current.Shutdown();
+        }
+
+        private void add_memo_click(object sender,EventArgs e)  //タスクバーの新規メモ作成ボタン
+        {
+            memo_window new_window = new memo_window();
+            new_window.Show();
+            memo_list.Add(new_window);
+        }
+    }
+}
