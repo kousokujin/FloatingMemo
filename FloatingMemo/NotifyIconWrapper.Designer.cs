51a740987008dc894ba1be5fc6e4ae9a45babbd8
kousokujin <tomonori05@gmail.com>
2015-10-18 21:00:53 +0900

    アイコン設定した

--- FloatingMemo/NotifyIconWrapper.Designer.cs
+++ FloatingMemo/NotifyIconWrapper.Designer.cs
@@ -30,30 +30,31 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotifyIconWrapper));
+            System.Windows.Forms.ContextMenuStrip notifyMenu;
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-            this.notifyMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.Add_new_memo = new System.Windows.Forms.ToolStripMenuItem();
-            this.exit_app = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.change_transportmode = new System.Windows.Forms.ToolStripMenuItem();
-            this.notifyMenu.SuspendLayout();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exit_app = new System.Windows.Forms.ToolStripMenuItem();
+            notifyMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            notifyMenu.SuspendLayout();
             // 
             // notifyIcon1
             // 
-            this.notifyIcon1.ContextMenuStrip = this.notifyMenu;
+            this.notifyIcon1.ContextMenuStrip = notifyMenu;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Text = "lentum";
             this.notifyIcon1.Visible = true;
             // 
             // notifyMenu
             // 
-            this.notifyMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            notifyMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.Add_new_memo,
             this.change_transportmode,
             this.toolStripSeparator1,
             this.exit_app});
-            this.notifyMenu.Name = "notifyMenu";
-            this.notifyMenu.Size = new System.Drawing.Size(156, 54);
+            notifyMenu.Name = "notifyMenu";
+            notifyMenu.Size = new System.Drawing.Size(156, 54);
             // 
             // Add_new_memo
             // 
@@ -62,17 +63,6 @@
             this.Add_new_memo.Size = new System.Drawing.Size(155, 22);
             this.Add_new_memo.Text = "新しいメモを追加";
             // 
-            // exit_app
-            // 
-            this.exit_app.Name = "exit_app";
-            this.exit_app.Size = new System.Drawing.Size(155, 22);
-            this.exit_app.Text = "終了";
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(152, 6);
-            // 
             // change_transportmode
             // 
             this.change_transportmode.CheckOnClick = true;
@@ -80,14 +70,24 @@
             this.change_transportmode.Size = new System.Drawing.Size(155, 22);
             this.change_transportmode.Text = "透過モード";
             this.change_transportmode.Click += new System.EventHandler(this.change_transportmode_Click);
-            this.notifyMenu.ResumeLayout(false);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(152, 6);
+            // 
+            // exit_app
+            // 
+            this.exit_app.Name = "exit_app";
+            this.exit_app.Size = new System.Drawing.Size(155, 22);
+            this.exit_app.Text = "終了";
+            notifyMenu.ResumeLayout(false);
 
         }
 
         #endregion
 
         private System.Windows.Forms.NotifyIcon notifyIcon1;
-        private System.Windows.Forms.ContextMenuStrip notifyMenu;
         private System.Windows.Forms.ToolStripMenuItem exit_app;
         private System.Windows.Forms.ToolStripMenuItem Add_new_memo;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
